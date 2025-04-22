f8f86ae232a0c723642c6818c0c43d0a83423fae
mbalike <dmbalike@outlook.com>
2025-04-22 13:42:44 +0300

    addded location

--- drivers2.php
+++ drivers2.php
@@ -430,6 +430,7 @@ $completed_count = mysqli_fetch_assoc(mysqli_query($conn, "SELECT COUNT(*) AS co
                             <td><strong><?= $row['id'] ?></strong></td>
                             <td><?= $row['name'] ?></td>
                             <td><?= $row['phone'] ?></td>
+                            <td><?= $row['location'] ?></td>
                             <td><?= $row['email'] ?></td>
                             <td>
                                 <?php
